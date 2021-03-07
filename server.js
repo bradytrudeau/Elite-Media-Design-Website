b213ece793d90213932b14fc93614239a1fcebfa
Brady Trudeau <bradytrudeau@gmail.com>
2021-03-07 15:16:18 -0600

    Changing deployment provider

--- server.js
+++ server.js
@@ -22,15 +22,8 @@ app.get('/categories', (req, res) => {
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
-if (process.env.NODE_ENV === "production") {
-  // Express will serve up production assets
-  app.use(express.static("build"));
+app.use(express.static('build'));
 
-  // Express will serve up the front-end index.html file if it doesn't recognize the route
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve("build", "index.html"))
-  );
-}
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
